01afee0354a069751b9242183f3e0506db7cb329
Jonathan Saji <jonathansaji2009@gmail.com>
2026-03-11 22:57:06 -0400

    updated batch file to create shortcut

--- start.bat
+++ start.bat
@@ -1,3 +1,14 @@
+@echo off
+
+:: Create desktop shortcut on first run
+set SHORTCUT=%USERPROFILE%\Desktop\SubSync.lnk
+set TARGET=%~dp0start.bat
+
+if not exist "%SHORTCUT%" (
+    powershell -Command "$ws = New-Object -ComObject WScript.Shell; $s = $ws.CreateShortcut('%SHORTCUT%'); $s.TargetPath = '%TARGET%'; $s.IconLocation = '%~dp0AppLogo.ico'; $s.Save()"
+    echo Shortcut created on Desktop!
+)
+
 start node server.js
 timeout /t 2
 start http://localhost:3000
